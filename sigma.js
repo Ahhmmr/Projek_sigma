8444a33c438ca61f83db083327ceef330f9afe42
SidqiMM <sidqimaanmuanas@gmail.com>
2026-02-09 19:05:31 +0700

    menambahkan bagian pahlawan Ngurah Rai & Pattimura

--- sigma.js
+++ sigma.js
@@ -4,85 +4,85 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 ScrollTrigger.create({
-    trigger: ".cover",
-    start: "top top",
-    end: "bottom top",
-    scrub: 0.5,
-    pin: true,
-    pinSpacing: false
+  trigger: ".cover",
+  start: "top top",
+  end: "bottom top",
+  scrub: 0.5,
+  pin: true,
+  pinSpacing: false
 
 });
 gsap.to(".one, .two", {
-    scrollTrigger: {
-        trigger: ".cover",
-        start: "top top",
-        end: "center top",
-        scrub: 1
-    },
-    opacity: 0,
-    y: -50,
-    filter: "blur(10px)"
+  scrollTrigger: {
+    trigger: ".cover",
+    start: "top top",
+    end: "center top",
+    scrub: 1
+  },
+  opacity: 0,
+  y: -50,
+  filter: "blur(10px)"
 });
 gsap.to(".fahli", {
-    scrollTrigger:{
-        trigger: ".cover",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-    },
-    filter: "blur(10px)",
-    scale: 1.1,
+  scrollTrigger: {
+    trigger: ".cover",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+  },
+  filter: "blur(10px)",
+  scale: 1.1,
 });
 
 gsap.from(".sur, .tp, .histo", {
-    scrollTrigger: {
-        trigger: ".expl",
-        start: "top 60%",
-        end: "center center",
-        scrub: 1,
-    },
-    y: 100,
-    opacity: 0,
-    filter: "blur(30px)",
-    stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".expl",
+    start: "top 60%",
+    end: "center center",
+    scrub: 1,
+  },
+  y: 100,
+  opacity: 0,
+  filter: "blur(30px)",
+  stagger: 0.2,
 });
 gsap.to(".tp, .histo", {
-    scrollTrigger: {
-        trigger: ".expl",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-    },
-    y: 100,
-    opacity: 0,
-    filter: "blur(30px)",
-    stagger: 0.2,
+  scrollTrigger: {
+    trigger: ".expl",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+  },
+  y: 100,
+  opacity: 0,
+  filter: "blur(30px)",
+  stagger: 0.2,
 });
 gsap.to(".sur", {
-    scrollTrigger:{
-        trigger: ".expl",
-        start: "top top",
-        end: "bottom top",
-        scrub: 1,
-    },
-    filter: "blur(10px)",
+  scrollTrigger: {
+    trigger: ".expl",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+  },
+  filter: "blur(10px)",
 });
 gsap.utils.toArray(".line").forEach((line, index) => {
-    gsap.fromTo(
-        line.querySelectorAll("span"),
-        {y: 50, opacity: 0},
-        {
-            y: 0,
-            opacity: 1,
-            duration: 0.7,
-            ease: "power3.out",
-            stagger: 0.5,
-            scrollTrigger: {
-                trigger: ".run",
-                start: "top 10%",
-                toggleActions: "play none play reverse"
-            },
-            delay: index * 0.5
-        }
-    );
+  gsap.fromTo(
+    line.querySelectorAll("span"),
+    { y: 50, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 0.7,
+      ease: "power3.out",
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".run",
+        start: "top 10%",
+        toggleActions: "play none play reverse"
+      },
+      delay: index * 0.5
+    }
+  );
 });
